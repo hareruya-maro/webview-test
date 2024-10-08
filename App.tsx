e55ce89046c7227778a258ff6dddf5e2a5b676f2
Haruya <h.nakamura@thedott.io>
2024-10-08 13:31:06 +0900

    テストアプリ作成

--- App.tsx
+++ App.tsx
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useRef, useState } from "react";
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import WebView, { WebViewMessageEvent } from "react-native-webview";
+
+// WebView console.log injection
+const debugging = `
+     // Debug
+     console = new Object();
+     console.log = function(log) {
+       window.webViewBridge.send("console", log);
+     };
+     console.debug = console.log;
+     console.info = console.log;
+     console.warn = console.log;
+     console.error = console.log;
+     `;
 
 export default function App() {
+  const [uri, setUri] = useState("");
+  const uriRef = useRef("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <SafeAreaView />
+      <View style={styles.uriInputArea}>
+        <Text>URL</Text>
+        <View style={{ flex: 1 }}>
+          <TextInput
+            onChangeText={(text) => (uriRef.current = text)}
+            style={styles.textInput}
+          />
+        </View>
+        <Button title="GO!" onPress={() => setUri(uriRef.current)} />
+      </View>
+      {uri && (
+        <WebView
+          source={{ uri }}
+          style={{ flex: 1 }}
+          injectedJavaScript={debugging}
+          onMessage={(event: WebViewMessageEvent) =>
+            console.log(event.nativeEvent.data)
+          }
+        />
+      )}
     </View>
   );
 }
@@ -13,8 +57,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  uriInputArea: { flexDirection: "row", alignItems: "center", padding: 10 },
+  textInput: {
+    backgroundColor: "#33333333",
+    margin: 10,
+    borderWidth: 0.5,
+    borderColor: "black",
+    padding: 5,
+    borderRadius: 5,
   },
 });
---
+++ README.md
@@ -0,0 +1,15 @@
+# Webview Test App
+
+WebViewでテストするためのアプリです。
+
+## 実行方法
+
+このリポジトリをcloneし、以下の手順を実施。
+※iOSのシミュレータがインストールされている前提
+
+```sh
+npx expo install
+npm run ios
+```
+
+アプリが起動したらTextInputにURLを入力し、GO!をクリックすればWebViewに表示されます。
